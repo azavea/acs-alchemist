528a83579ccaccbaa27faab669c98062e828da06
Bennet Huber <bhuber@azavea.com>
2012-03-30 18:20:18 -0400

    Fix jenkins test dll path

--- scripts/code_coverage/Jenkins_OpenCover_Vars.bat
+++ scripts/code_coverage/Jenkins_OpenCover_Vars.bat
@@ -5,6 +5,6 @@ set OPENCOVER_PATH="C:\software\OpenCover\OpenCover.Console.exe"
 set NUNIT=%nunit%
 set FILTER="+[Azavea.NijPredictivePolicing.*]* -[Azavea.NijPredictivePolicing.Test]*"
 set OPENCOVER_XML="results.xml"
-set DLLPATH="..\..\csharp\Azavea.NijPredictivePolicing.Test\bin\x86\Debug\Azavea.NijPredictivePolicing.Test.dll"
+set DLLPATH="..\..\csharp\Azavea.NijPredictivePolicing.Test\bin\Release\Azavea.NijPredictivePolicing.Test.dll"
 
 call %1
