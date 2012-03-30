99e1212a86f6538359bde0dca7fc02b73bdc330d
Bennet Huber <bhuber@azavea.com>
2012-03-30 18:12:46 -0400

    Customizing code coverage generation files for ACS

--- scripts/code_coverage/Jenkins_OpenCover_Vars.bat
+++ scripts/code_coverage/Jenkins_OpenCover_Vars.bat
@@ -3,8 +3,8 @@ REM Sets Jenkins partcover variables, and then calls the partcover command (assu
 
 set OPENCOVER_PATH="C:\software\OpenCover\OpenCover.Console.exe"
 set NUNIT=%nunit%
-set FILTER="+[Azavea.HunchLab.*]* -[Azavea.HunchLab.Test]* -[Azavea.HunchLab.Instancer]*"
+set FILTER="+[Azavea.NijPredictivePolicing.*]* -[Azavea.NijPredictivePolicing.Test]*"
 set OPENCOVER_XML="results.xml"
-set DLLPATH="..\..\csharp\Azavea.HunchLab.Test\bin\Release\Azavea.HunchLab.Test.dll"
+set DLLPATH="..\..\csharp\Azavea.NijPredictivePolicing.Test\bin\x86\Debug\Azavea.NijPredictivePolicing.Test.dll"
 
 call %1
